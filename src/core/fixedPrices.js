14e6f91977e331a82bb028e7df39ced665dd13b7
Karimsayed2002 <islamemadtwab@gmail.com>
2023-12-09 17:00:50 +0200

    Update

---
+++ src/core/fixedPrices.js
@@ -0,0 +1,32 @@
+export const prices = [
+  {
+    _id: 0,
+    name: 'Any',
+    array: [],
+  },
+  {
+    _id: 1,
+    name: '$100 to $200',
+    array: [100, 200],
+  },
+  {
+    _id: 2,
+    name: '$200 to $300',
+    array: [201, 300],
+  },
+  {
+    _id: 3,
+    name: '$300 to $400',
+    array: [301, 400],
+  },
+  {
+    _id: 4,
+    name: '$400 to $500',
+    array: [401, 500],
+  },
+  {
+    _id: 5,
+    name: 'More than 500',
+    array: [501, 1000],
+  },
+];
